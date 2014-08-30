e97a1bea49091f6027adeaa4f36010c6a9e499da
Lew <lewpiper@mac.com>
2014-08-30 10:21:16 -0700

    First Commit
    This is my first commit based on the tip calculator app off Ray
    Wenderlich’s examples.

---
+++ TipCalculator/TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,50 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Lew on 8/30/14.
+//  Copyright (c) 2014 Lew. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total:Double, taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func printPossibleTips() {
+        println("15%: \(calcTipWithTipPct(0.15))")
+        println("18%: \(calcTipWithTipPct(0.18))")
+        println("20%: \(calcTipWithTipPct(0.20))")
+    }
+
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+}
+
