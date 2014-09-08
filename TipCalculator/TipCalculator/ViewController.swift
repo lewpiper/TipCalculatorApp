524d6eb7232b8c727c654dcea6d18ff6ad46b180
Lew <lewpiper@mac.com>
2014-09-07 23:48:44 -0700

    First build and run
    This is the first build and run of the app. There are still several
    bugs including that it does not update the percentage and that it is
    having some issues with refreshing the UI after it is changed once
    again.

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    //Creating IBOutlets to connect up my UI with my code.
     @IBOutlet var totalTextField : UITextField!
     @IBOutlet var taxPctSlider : UISlider!
     @IBOutlet var taxPctLabel : UILabel!
     @IBOutlet var resultsTextView :UITextView!
-                            
+    
+    
+    //Connecting Outlets to a functions that need to be run.
+    @IBAction func calculateTapped(sender :AnyObject) {
+        tipCalc.total = Double((totalTextField.text as NSString).doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = " "
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        resultsTextView.text = results
+        
+    }
+    
+    
+    
+    @IBAction func taxPercentageChanged(sender :AnyObject) {
+        
+    }
+    
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+    
+    }
+
+    
+    
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI () {
+    totalTextField.text = String(format: "%0.2f", tipCalc.total)
+    taxPctSlider.value = Float(tipCalc.taxPct)*100
+    taxPctLabel.text = "Tax percent(\(Int(taxPctSlider.value))%)"
+    resultsTextView.text = " "
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
+         func taxPercentageChanged(sender : AnyObject) {
+            tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+            refreshUI()
+        }
+         func viewTapped(sender : AnyObject) {
+            totalTextField.resignFirstResponder()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +72,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
